df64babc18af17434b18fac82ce6815fa33a9ac8
ChoiSHy <tjdgus4697@gmail.com>
2024-10-05 04:04:12 +0900

    프로젝트 추가 및 클래스 다이어그램 작성중

---
+++ Travel-Scheduler/src/main/java/co/kr/travelscheduler/TravelSchedulerApplication.java
@@ -0,0 +1,13 @@
+package co.kr.travelscheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelSchedulerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TravelSchedulerApplication.class, args);
+    }
+
+}
---
+++ Travel-Scheduler/src/test/java/co/kr/travelscheduler/TravelSchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package co.kr.travelscheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelSchedulerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
